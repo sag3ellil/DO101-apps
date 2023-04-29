2b3514c974d3b31499a2efbe71e390c774d5bb95
sag3ellil <bouraoui.zich@gmail.com>
2023-04-29 11:32:56 +0200

    update app to version 2

--- version/app.js
+++ version/app.js
@@ -4,13 +4,10 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-
-    response = 'This is version 1 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
-});
+  response = 'This is version 2 of the app.' + '\n';
+  //send the response to the client
+  res.send(response);
+ });
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
