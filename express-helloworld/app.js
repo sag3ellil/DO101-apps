32e2d68afe3d73bfa285d81d22f7a4a8d0dc2fb1
sag3ellil <bouraoui.zich@gmail.com>
2023-04-29 11:33:28 +0200

    rollback

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,8 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
- response = 'This is version 2 of the app.' + '\n';
- //send the response to the client
- res.send(response);
+  res.send('Hello World!\n');
 });
-
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
